9ddc4c95e11ead1af41f4520e72fd35959057b35
Kakedragge <simonhst@stud.ntnu.no>
2017-10-27 11:55:08 +0200

    start stuff

---
+++ Assets/Scripts/AirManagement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AirManagement : MonoBehaviour {
+
+	public int stamina;
+	public Text staminaText;
+
+	private bool isDead = false;
+	private bool isWater = true;
+
+	// Use this for initialization
+	void Start () {
+		SetStaminaText ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (stamina >= 0) {
+			isDead = true;
+		}
+		if (isWater == true) {
+			stamina -= 1;
+		}
+		SetStaminaText ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.gameObject.CompareTag ("Air Pocket")) 
+		{
+			stamina = 1000;
+			isWater = false;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) 
+	{
+		if (other.gameObject.CompareTag ("Air Pocket")) 
+		{
+			isWater = true;
+		}
+	}
+
+	void SetStaminaText() {
+		staminaText.text = "Air: " + stamina.ToString();
+	}
+}
---
+++ Assets/Scripts/Swiming.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Swiming : MonoBehaviour {
+
+	public float speed = 2f;
+
+	Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		rb2D.velocity = new Vector2 (moveHorizontal * speed, moveVertical * speed);
+	}
+}
