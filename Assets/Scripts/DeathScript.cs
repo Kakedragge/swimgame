ffae2297893fbb97eb8cbc3728893a573a2528c7
Thomas Borresen <thomasbo@hotmail.no>
2017-11-08 15:16:17 +0100

    Edited scripts

--- Assets/Scripts/DeathScript.cs
+++ Assets/Scripts/DeathScript.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class DeathScript : MonoBehaviour {
 
+    private GameObject player;
+    private GameObject shark;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        shark = GameObject.FindGameObjectWithTag("Shark");
+        player = GameObject.FindGameObjectWithTag("player");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Shark"))
+        {
+            print("Collided with Shark");
+        }
+    }
 }
